cfeffd713d68b3434ef2de533bd8c7b7b1c54aba
Tejashwi Satyam <tejashwi.satyam.frontend@gmail.com>
2025-05-11 17:23:13 +0530

    further commit

--- frontend/src/Pages/LoginSinup.jsx
+++ frontend/src/Pages/LoginSinup.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../CSS/LoginSinup.css'
 
 const LoginSinup = () => {
     return (
-        <div>LoginSinup</div>
+        <div className='loginsinup'>
+            <div className="loginsinup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsinup-fields">
+                    <input type='text' placeholder='Your Name' />
+                    <input type='email' placeholder='Email Address' />
+                    <input type='password' placeholder='Password' />
+                    <button>Continue</button>
+                </div>
+
+                <p className="loginsinup-login">
+                    Already have an account ? <span>Login here</span>
+                </p>
+                <div className="loginsinup-agree">
+                    <input type='checkbox' name='' id='' />
+                    <p>By continuing, i agree to the terms of use & privacy policy.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
